f9f0718adecf17735aa6fe17c0eefd0d8531b759
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-02-12 16:09:30 +0100

    Add header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { SidenavListStaticComponent } from './navigation/sidenav-list-static/sidenav-list-static.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    SidenavListStaticComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation/sidenav-list-static/sidenav-list-static.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list-static',
+  templateUrl: './sidenav-list-static.component.html',
+  styleUrls: ['./sidenav-list-static.component.css']
+})
+export class SidenavListStaticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
